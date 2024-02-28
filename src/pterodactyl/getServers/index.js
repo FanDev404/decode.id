8eca4f9182a549c57580530c7b210edfe06cd7a2
Fan Dev <fandev404@gmail.com>
2024-02-28 19:20:35 +0700

    Add files via upload

--- src/pterodactyl/getServers/index.js
+++ src/pterodactyl/getServers/index.js
@@ -16,6 +16,7 @@ module.exports = async (server = "", metadata = { application_key: "", id: 1 }, 
                 if (status) {
                     results.data = data
                     results.status = true
+                    results.message = message
                 } else {
                     results.message = message
                 }
