ba8a0c816a28b52576cf26c9a081a82a26d16227
Fan Dev <fandev404@gmail.com>
2024-07-18 16:44:49 +0700

    Add files via upload

--- src/downloader/tiktok/index.js
+++ src/downloader/tiktok/index.js
@@ -1,4 +1,4 @@
-const { tiktokDLV1, tiktokDLV2 } = require("../../../lib/scrapper-download.js") 
+const { tiktokDLV1, tiktokDLV2, tiktokDLV3, tiktokDLV4 } = require("../../../lib/scrapper-download.js") 
 const { fetchBuffer } = require("../../../lib/function.js") 
 module.exports = async (url = "", options = { version: "v1" }) => {
     const image = []
@@ -12,7 +12,7 @@ module.exports = async (url = "", options = { version: "v1" }) => {
         results.message = "Link Url not valid"
         return results
     }
-    if (!Object.keys(options).includes("version") || options?.version !== "v1" && options?.version !== "v2") {
+    if (!Object.keys(options).includes("version") || options?.version !== "v1" && options?.version !== "v2" && options?.version !== "v3" && options?.version !== "v4") {
         results.message = "version not valid"
         return results
     }
@@ -20,35 +20,18 @@ module.exports = async (url = "", options = { version: "v1" }) => {
         case "v1":
             return await tiktokDLV1(url.trim()).then(async ({ status, data, message }) => {
                 if (status) {
-                    for (const x of data) {
-                        if (x.isWatermark == false && x.type == "video" && !Object.keys(results.data).includes(x.type)) {
-                            const { status, buffer } = await fetchBuffer(x.url)
-                            results.data["type"] = x.type
-                            if (status) {
-                                results.data[x.type] = {
-                                    "url": x.url, 
-                                    "buffer": buffer, 
-                                    "base64": buffer.toString("base64")
-                                }
-                            }
-                        } else if (x.isWatermark == false && x.type == "image") {
-                            const { status, buffer } = await fetchBuffer(x.url)
-                            if (status) image.push({ url: x.url, buffer, base64: buffer.toString("base64") })
-                        } else if (x.isWatermark == false && x.type == "audio" && !Object.keys(results.data).includes(x.type)) {
-                            const { status, buffer } = await fetchBuffer(x.url)
-                            if (status) {
-                                results.data[x.type] = {
-                                    "url": x.url, 
-                                    "buffer": buffer, 
-                                    "base64": buffer.toString("base64")
-                                }
-                            }
-                        }
-                    }
-                    if (image.length > 0) {
-                        results.data["type"] = "image"
-                        results.data["image"] = image
-                    }
+                    results.data = data
+                    results.status = true
+                } else {
+                    results.message = message
+                }
+                return results
+            })
+        break
+        case "v2":
+            return await tiktokDLV2(url.trim()).then(async ({ status, data, message }) => {
+                if (status) {
+                    results.data = data
                     results.status = true
                 } else {
                     results.message = message
@@ -56,38 +39,14 @@ module.exports = async (url = "", options = { version: "v1" }) => {
                 return results
             }) 
         break
-        case "v2":
-            return await tiktokDLV2(url.trim()).then(async ({ status, data, message }) => {
+        case "v3":
+            results.message = "Maitance....."
+            return results
+        break
+        case "v4":
+            return await tiktokDLV4(url.trim()).then(async ({ status, data, message }) => {
                 if (status) {
-                    for (const x of data) {
-                        if (x.type == "video" && !Object.keys(results.data).includes(x.type)) {
-                            const { status, buffer } = await fetchBuffer(x.url)
-                            results.data["type"] = x.type
-                            if (status) {
-                                results.data[x.type] = {
-                                    "url": x.url, 
-                                    "buffer": buffer, 
-                                    "base64": buffer.toString("base64")
-                                }
-                            }
-                        } else if (x.type == "image") {
-                            const { status, buffer } = await fetchBuffer(x.url)
-                            if (status) image.push({ url: x.url, buffer, base64: buffer.toString("base64") })
-                        } else if (x.type == "audio" && !Object.keys(results.data).includes(x.type)) {
-                            const { status, buffer } = await fetchBuffer(x.url)
-                            if (status) {
-                                results.data[x.type] = {
-                                    "url": x.url, 
-                                    "buffer": buffer, 
-                                    "base64": buffer.toString("base64")
-                                }
-                            }
-                        }
-                    }
-                    if (image.length > 0) {
-                        results.data["type"] = "image"
-                        results.data["image"] = image
-                    }
+                    results.data = data
                     results.status = true
                 } else {
                     results.message = message
