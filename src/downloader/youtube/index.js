c925e2e55021cdbc8ff41be42d08471781bda43b
Fan Dev <fandev404@gmail.com>
2024-07-19 14:26:19 +0700

    Add files via upload

--- src/downloader/youtube/index.js
+++ src/downloader/youtube/index.js
@@ -1,4 +1,4 @@
-const { youtubeDLV1 } = require("../../../lib/scrapper-download.js") 
+const { youtubeDLV1, youtubeDLV2 } = require("../../../lib/scrapper-download.js") 
 module.exports = async (url = "", options = { version: "v1" }) => {
     const results = { 
         "creator": "https://wa.me/6289674310267", 
@@ -10,13 +10,24 @@ module.exports = async (url = "", options = { version: "v1" }) => {
         results.message = "Link Url not valid"
         return results
     }
-    if (!Object.keys(options).includes("version") || options?.version !== "v1") {
+    if (!Object.keys(options).includes("version") || options?.version !== "v1" && options?.version !== "v2") {
         results.message = "version not valid"
         return results
     }
     switch (options?.version) {
         case "v1":
             return await youtubeDLV1(url.trim()).then(({ status, data, message }) => {
+                if (status) {
+                    results.data = data
+                    results.status = true
+                } else {
+                    results.message = message
+                }
+                return results
+            }) 
+        break
+        case "v2":
+            return await youtubeDLV2(url.trim()).then(({ status, data, message }) => {
                 if (status) {
                     results.data = data
                     results.status = true
