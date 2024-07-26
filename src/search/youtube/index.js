21442e02ca85511592414ac44996f52d5a10aa12
Fan Dev <fandev404@gmail.com>
2024-07-26 09:15:27 +0700

    Create index.js

---
+++ src/search/youtube/index.js
@@ -0,0 +1,35 @@
+const { youtubeSearchV1 } = require("../../../lib/scrapper-search.js") 
+module.exports = async (search = "", options = { version: "v1" }) => {
+    const results = { 
+        "creator": "https://wa.me/6289674310267", 
+        "status": false, 
+        "data": [], 
+        "message": ""
+    }
+    if (!search) {
+        results.message = "search not valid"
+        return results
+    }
+    if (!Object.keys(options).includes("version") || options?.version !== "v1") {
+        results.message = "version not valid"
+        return results
+    }
+    switch (options?.version) {
+        case "v1":
+            return await youtubeSearchV1(search.trim()).then(({ status, data, message }) => {
+                if (status) {
+                    results.data = data
+                    results.status = true
+                } else {
+                    results.message = message
+                }
+                return results
+            }) 
+        break
+        default: 
+        if (results.message == "") {
+            results.message = "Failed request"
+        }
+        return results
+    }
+}
