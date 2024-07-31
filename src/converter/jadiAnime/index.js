091d468467705e6fc950a3a7cdea56300ec7874d
Fan Dev <fandev404@gmail.com>
2024-07-31 19:43:44 +0700

    Add files via upload

--- src/converter/jadiAnime/index.js
+++ src/converter/jadiAnime/index.js
@@ -1,1 +1,36 @@
-
+const { isUrl } = require("../../../lib/function.js")
+const { jadiAnimeV1 } = require("../../../lib/scrapper-converter.js") 
+module.exports = async (content, options = { version: "v1" }) => {
+    const results = { 
+        "creator": "https://wa.me/6289674310267", 
+        "status": false, 
+        "data": "", 
+        "message": ""
+    }
+    if (!isUrl(content)) {
+        results.message = "content not valid"
+        return results
+    }
+    if (!Object.keys(options).includes("version") || options?.version !== "v1") {
+        results.message = "version not valid"
+        return results
+    }
+    switch (options?.version) {
+        case "v1":
+            return await jadiAnimeV1(content).then(({ status, data, message }) => {
+                if (status) {
+                    results.data = data
+                    results.status = true
+                } else {
+                    results.message = message
+                }
+                return results
+            }) 
+        break
+        default: 
+        if (results.message == "") {
+            results.message = "Failed request"
+        }
+        return results
+    }
+}
