ff93c05c76766fee7e5a917fcaf1817bc4670c2b
Fan Dev <fandev404@gmail.com>
2024-07-31 14:14:18 +0700

    Add files via upload

--- src/tools/chatGPT/index.js
+++ src/tools/chatGPT/index.js
@@ -1,5 +1,5 @@
-const { chatGPTV1 } = require("../../../lib/scrapper-tools.js") 
-module.exports = async (text = "", apikey = "", options = { version: "v1" }) => {
+const { chatGPTV1, chatGPTV2 } = require("../../../lib/scrapper-tools.js") 
+module.exports = async (text = "", options = { version: "v1" }) => {
     const results = { 
         "creator": "https://wa.me/6289674310267", 
         "status": false, 
@@ -9,17 +9,25 @@ module.exports = async (text = "", apikey = "", options = { version: "v1" }) => 
     if (!text) {
         results.message = "Message not valid"
         return results
-    } else if (!apikey) {
-        results.message = "Apikey not valid"
-        return results
-    }
-    if (!Object.keys(options).includes("version") || options?.version !== "v1") {
+    } 
+    if (!Object.keys(options).includes("version") || options?.version !== "v1" && options?.version !== "v2") {
         results.message = "version not valid"
         return results
     }
     switch (options?.version) {
         case "v1":
-            return await chatGPTV1(text.trim(), apikey).then(({ status, data, message }) => {
+            return await chatGPTV1(text.trim()).then(({ status, data, message }) => {
+                if (status) {
+                    results.data = data
+                    results.status = true
+                } else {
+                    results.message = message
+                }
+                return results
+            }) 
+        break
+        case "v2":
+            return await chatGPTV2(text.trim()).then(({ status, data, message }) => {
                 if (status) {
                     results.data = data
                     results.status = true
