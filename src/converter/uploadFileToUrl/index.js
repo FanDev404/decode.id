d9c2d018e492331c13b6012a99a4c3cdfc8d290c
Fan Dev <fandev404@gmail.com>
2024-07-31 14:12:35 +0700

    Add files via upload

--- src/converter/uploadFileToUrl/index.js
+++ src/converter/uploadFileToUrl/index.js
@@ -1,1 +1,36 @@
-
+const { isFiles } = require("../../../lib/function.js")
+const { uploadFileUrlV1 } = require("../../../lib/scrapper-converter.js") 
+module.exports = async (content, options = { version: "v1" }) => {
+    const results = { 
+        "creator": "https://wa.me/6289674310267", 
+        "status": false, 
+        "data": "", 
+        "message": ""
+    }
+    if (!(isFiles(content) == true)) {
+        results.message = "Files not valid"
+        return results
+    }
+    if (!Object.keys(options).includes("version") || options?.version !== "v1") {
+        results.message = "version not valid"
+        return results
+    }
+    switch (options?.version) {
+        case "v1":
+            return await uploadFileUrlV1(content).then(({ status, data, message }) => {
+                if (status) {
+                    results.data = data
+                    results.status = true
+                } else {
+                    results.message = message
+                }
+                return results
+            }) 
+        break
+        default: 
+        if (results.message == "") {
+            results.message = "Failed request"
+        }
+        return results
+    }
+}
